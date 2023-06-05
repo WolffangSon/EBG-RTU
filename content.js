4fc74713bd64439c61dac35ca5aaaa714e57ffef
Wolffang Son <wolfsonher@hotmail.com>
2023-06-04 23:34:17 -0600

    iniciando repositorio

---
+++ content.js
@@ -0,0 +1,36 @@
+chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+  switch (request.type) {
+    case "activate":
+      sendResponse({ status: "alive" });
+      break;
+    case "requestData":
+      let rows = constructData();
+      sendResponse({ status: "success", data: rows });
+      break;
+    default:
+      sendResponse({ status: "error" });
+  }
+});
+
+function constructData() {
+  let col = 0;
+  let rows = [];
+  if (jQuery) {
+    jQuery(
+      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7 .f9-widget-grid-contents-left docked div"
+    ).each(function (index) {
+      if (rows[col] == undefined) {
+        rows[col] = [];
+      }
+      rows[col].push($(this).text());
+      if ($(this).attr("class") == "lastCell") {
+        col++;
+      }
+    });
+  } else {
+    console.log("Error: jQuery library failed to load!");
+    return 0;
+  }
+
+  return rows;
+}
