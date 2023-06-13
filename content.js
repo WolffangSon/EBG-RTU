508ef2b654de9eaac89b1b434d993811bceb9035
Wolffang Nicolas Son Hernandez <wolffang.son@telusinternational.com>
2023-06-12 18:49:26 -0600

    Agregadas clases correctas

--- content.js
+++ content.js
@@ -19,13 +19,13 @@ function constructData() {
   let col = 0;
   if (jQuery) {
     jQuery(
-      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked'] div"
+      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked']"
     ).each(function (index) {
       if (rows[col] == undefined) {
         rows[col] = [];
       }
       rows[col].push($(this).text());
-      if ($(this).attr("class") == "f9-widget-grid-contents-center") {
+      if ($(this).attr("id") == "f9-widget-grid-row") {
         col++;
       }
     });
@@ -35,7 +35,7 @@ function constructData() {
   }
 
   return rows;
-}
+}//$('[id^="foo"]')
 
 /*function constructTable(){
   let table=[];
