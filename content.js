2520b37c6170f1c3a5d9a21d1853bd3edeed63ab
Wolffang Son <wolfsonher@hotmail.com>
2023-06-13 22:37:23 -0600

    pruebas con salto de carro

--- content.js
+++ content.js
@@ -13,19 +13,25 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
 });
 
 function constructData() {
-  table =[]
+  table = [];
   //let col = 0;
   let rows = [];
   let col = 0;
   if (jQuery) {
     jQuery(
-      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked'] div"
+      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked']"
     ).each(function (index) {
       if (rows[col] == undefined) {
         rows[col] = [];
       }
-      rows[col].push($(this).text());
-      if ($(this).attr("class") == "f9-widget-grid-contents-center") {
+      rows[col].push(
+        $(this).text() +
+          "\n" +
+          "Salto de Carro " +
+          "la Clase parent es: " +
+          $(this).attr("class")
+      );
+      if ($(this).attr("class") == "f9-widget-grid-row") {
         col++;
       }
     });
@@ -36,22 +42,6 @@ function constructData() {
 
   return rows;
 }
-
-/*function constructTable(){
-  let table=[];
-  let col=0;
-  let row=0;
-  if (jQuery){// User name and timestamp array
-    jQuery("#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked'] div")
-    if(table[row]==undefined){
-      table[row]=[];
-    }
-    table[row].push($(this).text)
-    col ++;
-    jQuery("#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-center'] div[class='f9-widget-grid-contents-center-inner'] div")
-    
-  }
-}*/
 
 /*function constructData() {
   let table = [];
