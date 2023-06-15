94b2c00d0d922918826dfa3060b18a8094ca8e0d
Wolffang Nicolas Son Hernandez <wolffang.son@telusinternational.com>
2023-06-15 16:23:42 -0600

    agregada la clase de objetos con informacion de log

--- content.js
+++ content.js
@@ -12,24 +12,22 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   }
 });
 
-
-
 function constructData() {
    let rows = [];
   let col = 0;
   let row =0;
   if (jQuery) {
     jQuery(
-      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked'] div[class='f9-widget-grid-row']"
+      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-center'] div[class='f9-widget-grid-contents-center-inner'] div[class^='f9-widget-grid-row']"
     ).each(function (index) {
       if (rows[row] == undefined) {
         rows[row] = [];
       }
       rows[row].push(
         $(this).text()
-         + "\n" + "La clase actual es: " + $(this).attr("class")
+        // + "\n" + "La clase actual es: " + $(this).attr("class")
       );
-      if ($(this).attr("class") == "f9-widget-grid-row") {
+      if ($(this).attr("class").startsWith("f9-widget-grid-row")) {
         row++;
       }
     });
