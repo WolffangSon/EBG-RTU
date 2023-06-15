cd540336503c88d5ba64fd5171f7adc02a15c818
Wolffang Nicolas Son Hernandez <wolffang.son@telusinternational.com>
2023-06-15 12:18:22 -0600

    clean data

--- content.js
+++ content.js
@@ -12,107 +12,41 @@ chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
   }
 });
 
+
+
 function constructData() {
-  var table = [];
-  let row = 0;
+   let rows = [];
   let col = 0;
-
-  if (typeof jQuery !== "undefined") {
-    jQuery(
-      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked'] div[class='f9-widget-grid-row']"
-    ).each(function (index) {
-      if (table[row] == undefined) {
-        table[row] = [];
-      }
-
-      //.push($(this).text());
-      table[row][col] = {
-        text: $(this).text(),
-        class: $(this).attr("class"),
-      };
-
-      row++;
-
-      if ($(this).attr("class") == "f9-widget-grid-row") {
-        col++;
-      }
-    });
-
-    jQuery(
-      "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-center'] div[class='f9-widget-grid-contents-center'] div[class='f9-widget-grid-contents-center-inner'] div[class='f9-widget-grid-row']"
-    ).each(function (index) {
-      table[row][1] = {
-        text: $(this).text(),
-        class: $(this).attr("class"),
-      };
-      col++;
-    });
-  } else {
-    console.log("Error: jQuery library failed to load!");
-    return null;
-  }
-
-  return table;
-}
-
-/*function constructData() {
-  table = [];
-  //let col = 0;
-  let rows = [];
-  let col = 0;
+  let row =0;
   if (jQuery) {
     jQuery(
       "#f7808fdf-c04b-45ea-9e0b-ec8850b86db7:last div[class='f9-widget-grid-content'] div[class='f9-widget-grid-content-wrapper'] div[class='f9-widget-grid-content-inner'] div[class='f9-widget-grid-contents-left docked'] div[class='f9-widget-grid-row']"
     ).each(function (index) {
-      if (rows[col] == undefined) {
-        rows[col] = [];
+      if (rows[row] == undefined) {
+        rows[row] = [];
       }
-      rows[col].push(
+      rows[row].push(
         $(this).text()
          + "\n" + "La clase actual es: " + $(this).attr("class")
       );
-      if ($(this).attr("class") == "f9-widget-grid-contents-center") {
-        col++;
+      if ($(this).attr("class") == "f9-widget-grid-row") {
+        row++;
       }
     });
+
+
   } else {
     console.log("Error: jQuery library failed to load!");
     return 0;
   }
 
   return rows;
-} */
+} 
 
-//$('[id^="foo"]')
 
 /*
 https://script.google.com/a/macros/noom.com/s/AKfycbw8lRty-W4KAMyBLr3MdxtCGwobVldN4tzSSiH_ydFshhZM4FnaIj6cZ4PuCiXnw75weg/exec
 https://script.google.com/a/macros/telusinternational.com/s/AKfycbwzaEro4pUxG9ftswFAY-RQVyVbI5T9zg1P4P6CTh4rJTyYefLb4nxB538mK56rGvlwYA/exec
 wolffang.son@telusinternational.com
-EBG Raw
-function constructData() {
-  let table = [];
-  let col = 0;
-  let row = 0;
-  if(jQuery) {
-    jQuery("#oRightPaneContent").contents().find("#adherenceListWrapper .tblItem").each(function(index) {
-      if(table[row] == undefined) {
-        table[row] = [];
-      }
+EBG Raw*/
 
-      table[row].push($(this).text());
-      col ++;
-
-      if(col > 7) {
-        row ++;
-        col = 0;
-      }
-    });
-  } else {
-    console.log('Error: jQuery library failed to load!');
-    return 0;
-  }
-
-  return table;
-
-}*/
